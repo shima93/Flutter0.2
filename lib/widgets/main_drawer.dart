42eab39ef79e20bad2faff47437dae43d7c3d4d0
Shima93 <shimakeramatim@gmail.com>
2022-06-01 09:39:18 -0400

    favorite page

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -40,10 +40,10 @@ class MainDrawer extends StatelessWidget {
             height: 20,
           ),
           buildListTile('Meals', Icons.restaurant_outlined, () {
-            Navigator.of(context).pushNamed('/');
+            Navigator.of(context).pushReplacementNamed('/');
           }),
           buildListTile('Settings', Icons.settings, () {
-            Navigator.of(context).pushNamed(FiltersScreen.routeName);
+            Navigator.of(context).pushReplacementNamed(FiltersScreen.routeName);
           })
         ],
       ),
