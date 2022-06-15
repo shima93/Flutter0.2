53c29f9970c30d260e3cf79ba81249aec4c25316
Shima93 <shimakeramatim@gmail.com>
2022-06-15 16:36:57 -0400

    adding filter screen

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,21 @@ class _MyAppState extends State<MyApp> {
   void _setFilters(Map<String, bool> filterData) {
     setState(() {
       _filters = filterData;
-      _availableMeals = DUMMY_MEALS.where((meal) => false).toList();
+      _availableMeals = DUMMY_MEALS.where((meal) {
+        if (_filters['gluten'] && !meal.isGlutenFree) {
+          return false;
+        }
+        if (_filters['lactose'] && !meal.isLactoseFree) {
+          return false;
+        }
+        if (_filters['vegan'] && !meal.isVegan) {
+          return false;
+        }
+        if (_filters['vegetarian'] && !meal.isVegetarian) {
+          return false;
+        }
+        return true;
+      }).toList();
     });
   }
 
